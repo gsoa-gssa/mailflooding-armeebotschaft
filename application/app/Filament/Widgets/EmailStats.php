0eadf9316660dc66a53f56c8358423fd50494333
Timothy Justin Oesch <54284649+timothyoesch@users.noreply.github.com>
2024-05-29 20:17:14 +0200

    Add Stats widget to frontpage

---
+++ application/app/Filament/Widgets/EmailStats.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Email;
+use App\Models\Contact;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class EmailStats extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        $sentEmails = Email::whereNot('sent_at', null)->count();
+        $sentEmailsWithin30Minutes = Email::where('sent_at', '>', now()->subMinutes(30))->count();
+        $contacts = Contact::count();
+        $optinContacts = Contact::where('optin', true)->count();
+        return [
+            Stat::make('Sent Emails', $sentEmails),
+            Stat::make('Sent Emails within 30 minutes', $sentEmailsWithin30Minutes),
+            Stat::make('Contacts', $contacts),
+            Stat::make('Opt-in Contacts', $optinContacts),
+        ];
+    }
+}
